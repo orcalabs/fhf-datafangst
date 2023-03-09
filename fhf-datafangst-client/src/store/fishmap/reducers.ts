156459dd72448cde6104abe8c6fc2c2057e36354
Kim Hartvedt Andreassen <kim@orcalabs.no>
2023-03-09 14:06:44 +0100

    refactor: move selected grid logic to state

--- fhf-datafangst-client/src/store/fishmap/reducers.ts
+++ fhf-datafangst-client/src/store/fishmap/reducers.ts
@@ -1,6 +1,7 @@
 import { ActionReducerMapBuilder } from "@reduxjs/toolkit";
+import { Stroke, Style } from "ol/style";
 import { AppState } from "store/state";
-import { initializeMap, setSelectedGrids } from ".";
+import { initializeMap, toggleSelectedArea } from ".";
 
 export const fishmapBuilder = (
   builder: ActionReducerMapBuilder<AppState>,
@@ -9,9 +10,27 @@ export const fishmapBuilder = (
     .addCase(initializeMap, (state, action) => {
       state.map = action.payload;
     })
-    .addCase(setSelectedGrids, (state, action) => {
+    .addCase(toggleSelectedArea, (state, action) => {
+      const selected = [...state.selectedGrids];
+      const area = action.payload;
+      const index = selected.findIndex(
+        (a) => a.get("objectid") === area.get("objectid"),
+      );
+      if (index < 0) {
+        const style = area.getStyle() as Style;
+        style.setStroke(new Stroke({ color: "#ffffff", width: 2 }));
+        style.setZIndex(1000);
+        area.changed();
+        selected.push(area);
+      } else {
+        const removed = selected.splice(index, 1);
+        const style = removed[0].getStyle() as Style;
+        style.setStroke(new Stroke({ color: "rgba(255, 0, 0, 0)" }));
+        removed[0].changed();
+      }
+
       return {
         ...state,
-        selectedGrids: action.payload,
+        selectedGrids: selected,
       };
     });
