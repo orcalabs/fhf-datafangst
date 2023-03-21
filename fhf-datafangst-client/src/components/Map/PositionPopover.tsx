6076362f845411ec8a1a0ab17e80195c7168131c
Kim Hartvedt Andreassen <kim@orcalabs.no>
2023-03-21 15:10:31 +0100

    feat: add hover on map objects

---
+++ fhf-datafangst-client/src/components/Map/PositionPopover.tsx
@@ -0,0 +1,36 @@
+import { FC } from "react";
+import { Box, List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
+import { dateFormat } from "utils";
+import { AisPosition } from "generated/openapi";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+
+interface Props {
+  hoveredPosition: AisPosition;
+}
+
+export const PositionPopover: FC<Props> = ({ hoveredPosition }) => {
+  if (!hoveredPosition.det) {
+    return <> </>;
+  }
+  return (
+    <Box sx={{ pr: 1, pl: 0.5 }}>
+      <List dense disablePadding>
+        <ListItem disableGutters>
+          <ListItemIcon sx={{ minWidth: 0, pr: 1.5, pl: 0 }}>
+            <LocationOnIcon fontSize="large" sx={{ color: "third.main" }} />
+          </ListItemIcon>
+          <ListItemText
+            primary={dateFormat(hoveredPosition.timestamp, "d MMM p")}
+            secondary={
+              hoveredPosition.det.speedOverGround
+                ? Number(hoveredPosition.det.speedOverGround).toFixed(1) +
+                  " knop"
+                : "Ukjent fart"
+            }
+            secondaryTypographyProps={{ color: "primary" }}
+          />
+        </ListItem>
+      </List>
+    </Box>
+  );
+};
