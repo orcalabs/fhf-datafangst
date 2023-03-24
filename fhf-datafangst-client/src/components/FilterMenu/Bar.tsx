376ad5fe57de4caa8335f0fa480eabdd545a14b8
Kim Hartvedt Andreassen <kim@orcalabs.no>
2023-03-24 14:38:33 +0100

    refactor: add bars to species filters

--- fhf-datafangst-client/src/components/FilterMenu/Bar.tsx
+++ fhf-datafangst-client/src/components/FilterMenu/Bar.tsx
@@ -14,7 +14,7 @@ export const Bar: FC<Props> = (props) => {
 
   return (
     <Box sx={{ display: "flex", mb: 1 }}>
-      <Box sx={{ width: 120, fontSize: 13 }}>{label}</Box>
+      <Box sx={{ width: 220, fontSize: 13 }}>{label}</Box>
       <Box
         sx={{
           width: "100%",
--- fhf-datafangst-client/src/components/FilterMenu/SpecieFilter.tsx
+++ fhf-datafangst-client/src/components/FilterMenu/SpecieFilter.tsx
@@ -1,9 +1,12 @@
 import { FC } from "react";
-import { Autocomplete, Box, TextField, Typography } from "@mui/material";
-import { StyledPopper } from "components";
-import DisabledByDefaultIcon from "@mui/icons-material/DisabledByDefault";
-import { selectSpeciesGroups, useAppSelector } from "store";
+import { Box, Typography } from "@mui/material";
+import {
+  selectSpeciesFilterStats,
+  selectSpeciesGroups,
+  useAppSelector,
+} from "store";
 import { SpeciesGroup } from "generated/openapi";
+import { Bar } from "./Bar";
 
 interface Props {
   value?: SpeciesGroup[];
@@ -11,45 +14,65 @@ interface Props {
 }
 
 export const SpecieFilter: FC<Props> = (props) => {
-  const { value, onChange } = props;
   const speciesGroups = useAppSelector(selectSpeciesGroups);
+  const speciesFilterStats = useAppSelector(selectSpeciesFilterStats);
+  const value = props.value ?? [];
+
+  const onChange = (value: SpeciesGroup[]) =>
+    props.onChange(value.length ? value : undefined);
+
+  const total: number = speciesFilterStats.reduce((acc: number, obj) => {
+    return acc + obj.value;
+  }, 0);
+
+  const handleClick = (id: number) => {
+    const speciesGroup = speciesGroups[id];
+    if (speciesGroup) {
+      if (value.some((speciesGroup) => speciesGroup.id === id)) {
+        onChange(value.filter((lg) => lg.id !== id));
+      } else {
+        onChange([...value, speciesGroup]);
+      }
+    }
+  };
+
+  if (!speciesFilterStats.length) {
+    return <></>;
+  }
 
   return (
-    <Box
-      sx={{
-        "& .MuiChip-filled": {
-          color: "black",
-          bgcolor: "secondary.main",
-          borderRadius: 0,
-        },
-        "& .MuiButtonBase-root": {
-          borderRadius: 0,
-
-          "&:hover": {
-            borderRadius: 0,
-          },
-        },
-      }}
-    >
+    <>
       <Typography sx={{ pb: 1, pt: 2 }} fontWeight="bold">
         Art
       </Typography>
-      <Autocomplete
-        multiple
-        ChipProps={{ deleteIcon: <DisabledByDefaultIcon /> }}
-        size="small"
-        blurOnSelect
-        PopperComponent={StyledPopper}
-        limitTags={3}
-        disablePortal
-        disableListWrap
-        onKeyDown={(e) => e.stopPropagation()}
-        value={value ?? []}
-        onChange={(_, value) => onChange(value?.length ? value : undefined)}
-        options={speciesGroups}
-        getOptionLabel={(option: SpeciesGroup) => option.name}
-        renderInput={(params: any) => <TextField {...params} />}
-      />
-    </Box>
+      <Box>
+        {[...speciesFilterStats]
+          .sort((a, b) => b.value - a.value)
+          .map((val, i) => {
+            const barLength = (val.value / total) * 100;
+
+            return (
+              <Box
+                key={i}
+                sx={{
+                  ":hover": {
+                    cursor: "pointer",
+                  },
+                }}
+                onClick={() => handleClick(val.id)}
+              >
+                <Bar
+                  length={barLength}
+                  label={speciesGroups[val.id].name}
+                  value={val.value}
+                  selected={value.some(
+                    (lengthGroup) => lengthGroup.id === val.id,
+                  )}
+                />
+              </Box>
+            );
+          })}
+      </Box>
+    </>
   );
 };
