e7635fde6c27fad16008eef4749c4f330ae5f661
Kim Hartvedt Andreassen <kim@orcalabs.no>
2023-05-26 09:14:31 +0200

    refactor: split logic between trip and hauls track

--- fhf-datafangst-client/src/components/Layers/TripsLayer.tsx
+++ fhf-datafangst-client/src/components/Layers/TripsLayer.tsx
@@ -1,24 +1,30 @@
 import { FC, useEffect, useState } from "react";
 import { VectorLayer } from "components";
-import { generateVesselTrackVector, TravelVector } from "utils";
+import {
+  generateTripHaulsVector,
+  generateVesselTrackVector,
+  TravelVector,
+} from "utils";
 import {
   selectFishmapState,
-  selectSelectedHaul,
   selectSelectedTrip,
   selectTrack,
   useAppSelector,
 } from "store";
+import VectorSource from "ol/source/Vector";
+import { Point } from "ol/geom";
 
-export const TrackLayer: FC = () => {
+export const TripsLayer: FC = () => {
   const track = useAppSelector(selectTrack);
   const state = useAppSelector(selectFishmapState);
-  const haul = useAppSelector(selectSelectedHaul);
   const trip = useAppSelector(selectSelectedTrip);
 
   const [zoom, setZoom] = useState<number | undefined>(
     state.map.getView().getZoom(),
   );
   const [trackVectors, setTrackVectors] = useState<TravelVector[]>();
+  const [haulsVector, setHaulsVector] = useState<VectorSource<Point>>();
+
   // Store map zoom level in state
   useEffect(() => {
     state.map.on("moveend", function () {
@@ -30,17 +36,21 @@ export const TrackLayer: FC = () => {
   }, [state.map]);
 
   useEffect(() => {
-    const vec = trip
-      ? generateVesselTrackVector(track, zoom, undefined)
-      : generateVesselTrackVector(track, zoom, haul);
-    setTrackVectors(vec);
-  }, [track, zoom]);
+    if (trip) {
+      const vec = generateVesselTrackVector(track, zoom, undefined, true);
+      const haulsVec = generateTripHaulsVector(trip, zoom);
+
+      setTrackVectors(vec);
+      setHaulsVector(haulsVec);
+    }
+  }, [track, zoom, trip]);
 
   return (
     <>
       {trackVectors?.map((v, i) => (
         <VectorLayer key={i} source={v.vector} zIndex={6} style={v.style} />
       ))}
+      <VectorLayer source={haulsVector} zIndex={7} />
     </>
   );
 };
