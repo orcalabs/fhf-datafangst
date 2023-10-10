9822eb4830c1dda49dbceac7ff825338833fe4a3
Vetle Hofsøy-Woie <vetle.hofsoy-woie@sintef.no>
2023-10-10 12:46:08 +0200

    Cards with real time data for self

--- fhf-datafangst-client/src/components/BenchmarkData/BenchmarkCards.tsx
+++ fhf-datafangst-client/src/components/BenchmarkData/BenchmarkCards.tsx
@@ -6,8 +6,17 @@ import { selectTrips, useAppSelector } from "store";
 import { Trips } from "components/MainMenu/Trips";
 import { Grid } from "@mui/material";
 
-export const BenchmarkCards: FC = (props) => {
-  const trips = useAppSelector(selectTrips);
+
+export const BenchmarkCards: FC= (props) => {
+
+  
+  const trips = useAppSelector(selectTrips)
+  if (!trips){
+    return <></>
+  }
+  const latest = trips[0];
+  const prev = trips[1];
+  
 
   return (
     <Grid
@@ -18,7 +27,23 @@ export const BenchmarkCards: FC = (props) => {
    >
     <Grid item xs={3}>
       <Box>
-        <BenchmarkCard title = "Total time" value={450} secondary_value={337}/>
+        <BenchmarkCard title = "Total time" 
+        value={
+          ((new Date(latest.end)).getTime() - (new Date(latest.start).getTime())) / (1000 * 3600 * 24)
+        } 
+        description="Latest trip"
+        secondary_value={
+          ((new Date(prev.end)).getTime() - (new Date(prev.start).getTime())) / (1000 * 3600 * 24)
+        }
+        secondary_description="Previous trip"
+
+        third_value={
+          5
+        }
+        third_description="Avarage friend trips"
+        metric="days"
+        tooltip="Calculated based on your last por and dep message"
+        />
       </Box>
     </Grid>
     <Grid item xs={3}>
