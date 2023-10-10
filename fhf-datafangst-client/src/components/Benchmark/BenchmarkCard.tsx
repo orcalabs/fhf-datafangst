4e3a94a826488a43a25381bafdffe32fa3a1d016
Vetle Hofsøy-Woie <vetle.hofsoy-woie@sintef.no>
2023-10-10 10:32:40 +0200

    Benchmark cards

--- fhf-datafangst-client/src/components/Benchmark/BenchmarkCard.tsx
+++ fhf-datafangst-client/src/components/Benchmark/BenchmarkCard.tsx
@@ -1,91 +1,44 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import { CardActionArea, CardHeader, Grid, Tooltip } from "@mui/material";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { CardHeader, Tooltip } from "@mui/material";
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
 interface BenchmarkCardProps {
-  title: string;
-  value: number | string;
-  description?: string;
-  primary_color?: string;
-  metric?: string;
-  secondary_value?: number | string;
-  tooltip?: string;
-  secondary_description?: string;
-  third_value?: number;
-  third_description?: string;
-  avatar: any;
-  onClick?: () => void;
+    title : string;
+    value : number;
+    secondary_value : number;
 }
 
 export const BenchmarkCard: FC<BenchmarkCardProps> = (props) => {
+
   return (
-    <Tooltip title={props.tooltip}>
-      <Card
-        variant="outlined"
-        style={{ margin: "auto", backgroundColor: "#067593" }}
-      >
-        <CardActionArea onClick={props.onClick}>
-          <CardHeader
-            avatar={props.avatar}
-            title={props.title}
-            titleTypographyProps={{ variant: "h3", color: "text.secondary" }}
-          />
-          <CardContent>
-            <Grid container spacing={0}>
-              {props.description && (
-                <Grid item xs={12}>
-                  <Typography color="text.secondary">
-                    {props.description}
-                  </Typography>
-                </Grid>
-              )}
-              <Grid item xs={12} alignItems="center" justifyContent="center">
-                <Typography
-                  variant="h1"
-                  color={
-                    props.primary_color ? props.primary_color : "text.primary"
-                  }
-                  component="div"
-                  align="center"
-                >
-                  {props.value} {props.metric ?? ""}
-                </Typography>
-              </Grid>
-              {props.secondary_description && (
-                <Grid item xs={6}>
-                  <Typography color="text.secondary">
-                    {props.secondary_description}
-                  </Typography>
-                </Grid>
-              )}
-              {props.secondary_description && (
-                <Grid item xs={6}>
-                  <Typography variant="h4" color="white">
-                    {props.secondary_value} {props.metric}
-                  </Typography>
-                </Grid>
-              )}
-              {props.third_description && (
-                <Grid item xs={3}>
-                  <Typography color="text.secondary">
-                    {props.third_description}
-                  </Typography>
-                </Grid>
-              )}
-              {props.third_value && (
-                <Grid item xs={3}>
-                  <Typography variant="h4">
-                    {props.third_value.toFixed(2)} {props.metric}
-                  </Typography>
-                </Grid>
-              )}
-            </Grid>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+    <Tooltip title="Hello world">
+    <Card  variant="outlined" color="primary">
+        <CardHeader
+            avatar = {<AccessTimeIcon/>}
+            title = {props.title}
+        />
+        <CardContent>
+        <Typography variant="h5" color="text.primary" component="div" sx={{fontSize: 30}}>
+            {props.value}
+        </Typography>
+        <Typography variant="body2" color="text.secondary"> 
+            Avg last x trips
+        </Typography>
+        <Typography variant="body2">
+            {props.secondary_value}
+        </Typography>
+        </CardContent>
+        <CardActions>
+        <Button size="small">Info</Button>
+        </CardActions>
+    </Card>
     </Tooltip>
   );
 };
--- fhf-datafangst-client/src/components/Benchmark/BenchmarkCards.tsx
+++ fhf-datafangst-client/src/components/Benchmark/BenchmarkCards.tsx
@@ -1,225 +1,41 @@
-import { FC } from "react";
-import Box from "@mui/material/Box";
+import { FC, useEffect } from "react";
+
+import Box from '@mui/material/Box';
 import { BenchmarkCard } from "./BenchmarkCard";
-import { selectTrips, useAppDispatch, useAppSelector } from "store";
+import { selectTrips, useAppSelector } from "store";
+import { Trips } from "components/MainMenu/Trips";
 import { Grid } from "@mui/material";
-import { BenchmarkModalParams, setBenchmarkModal } from "store/benchmark";
-import { Trip } from "generated/openapi";
-import { BenchmarkModal } from "./BenchmarkModal";
-import ScaleRoundedIcon from "@mui/icons-material/ScaleRounded";
-import StraightenRoundedIcon from "@mui/icons-material/StraightenRounded";
-import AccessTimeIcon from "@mui/icons-material/AccessTime";
-import PhishingRoundedIcon from "@mui/icons-material/PhishingRounded";
-import { createDurationFromHours } from "utils";
 
-const getTotalTimes = (trips: Trip[]) =>
-  trips.map(
-    (t) =>
-      (new Date(t.end).getTime() - new Date(t.start).getTime()) / 3_600_000,
-  );
 
-const getFishingHours = (trips: Trip[]) =>
-  trips.map((t) =>
-    t.hauls.reduce(
-      (tot, h) =>
-        tot +
-        (new Date(h.stopTimestamp).getTime() -
-          new Date(h.startTimestamp).getTime()) /
-          3_600_000,
-      0,
-    ),
-  );
+export const BenchmarkCards: FC= (props) => {
 
-const getFishingDistance = (trips: Trip[]) =>
-  trips.map((t) => t.hauls.reduce((tot, h) => tot + (h.haulDistance ?? 0), 0));
+  
+  const trips = useAppSelector(selectTrips)
 
-const getFishingWeight = (trips: Trip[]) =>
-  trips.map((t) => t.delivery.totalLivingWeight);
-
-const getTripDates = (trips: Trip[]) => trips.map((t) => t.start);
-
-enum BenchmarkType {
-  TotalTime,
-  FishingHours,
-  FishingDistance,
-  FishingWeight,
-}
-
-export const BenchmarkCards: FC = () => {
-  const dispatch = useAppDispatch();
-  const trips = useAppSelector(selectTrips);
-  if (!trips) {
-    return <></>;
-  }
-
-  const totalTimes = getTotalTimes(trips);
-  const fishingHours = getFishingHours(trips);
-  const fishingDistance = getFishingDistance(trips);
-  const fishingWeight = getFishingWeight(trips);
-  const totalTimeMean =
-    totalTimes.reduce((a, b) => a + b, 0) / totalTimes.length;
-  const fishingHoursMean =
-    fishingHours.reduce((a, b) => a + b, 0) / fishingHours.length;
-  const fishingDistanceMean =
-    fishingDistance.reduce((a, b) => a + b, 0) / fishingDistance.length;
-  const fishingWeightMean =
-    fishingWeight.reduce((a, b) => a + b, 0) / fishingWeight.length;
-
-  const xAxis = getTripDates(trips);
-  const modalParams: Record<BenchmarkType, BenchmarkModalParams> = {
-    [BenchmarkType.TotalTime]: {
-      title: "Total tid",
-      description:
-        "Total tid er regnet som tiden mellom havneavgang og havneanløp.",
-      yAxis:
-        totalTimeMean > 24
-          ? totalTimes.map((t) => Number((t / 24).toFixed(2)))
-          : totalTimes.map((t) => Number(t.toFixed(2))),
-      metric: totalTimeMean > 24 ? "Dager" : "Timer",
-      xAxis,
-    },
-    [BenchmarkType.FishingHours]: {
-      title: "Fisketid",
-      description:
-        "Fisketid er regnet som summen av tiden brukt under hver fangstmelding.",
-      metric: fishingHoursMean > 24 ? "Dager" : "Timer",
-      yAxis:
-        fishingHoursMean > 24
-          ? fishingHours.map((t) => Number((t / 24).toFixed(2)))
-          : fishingHours.map((t) => Number(t.toFixed(2))),
-      xAxis,
-    },
-
-    [BenchmarkType.FishingDistance]: {
-      title: "Fiskedistanse",
-      description:
-        "Fiskedistanse er regnet ut basert på VMS og AIS-meldingene som ble sendt under hver fangstmelding.",
-      metric: fishingDistanceMean > 1852 ? "Nautiske mil" : "Meter",
-      yAxis:
-        fishingDistanceMean > 1852
-          ? fishingDistance.map((d) => Number((d / 1852).toFixed(2)))
-          : fishingDistance.map((d) => Number(d.toFixed(2))),
-      xAxis,
-    },
-    [BenchmarkType.FishingWeight]: {
-      title: "Total vekt",
-      description: "Total vekt er basert på total landet vekt",
-      metric: fishingWeightMean > 1000 ? "Tonn" : "Kilo",
-      yAxis:
-        fishingWeightMean > 1000
-          ? fishingWeight.map((w) => Number((w / 1000).toFixed(2)))
-          : fishingWeight.map((w) => Number(w.toFixed(2))),
-      xAxis,
-    },
-  };
-
-  const handleClick = (type: BenchmarkType) => {
-    const modal = {
-      ...modalParams[type],
-    };
-
-    dispatch(setBenchmarkModal(modal));
-  };
 
   return (
     <Grid
-      container
-      spacing={3}
-      sx={{ padding: 3, backgroundColor: "primary.main" }}
-    >
-      <Grid item xs={6}>
-        <Box>
-          <BenchmarkCard
-            title="Total tid"
-            avatar={<AccessTimeIcon sx={{ color: "text.secondary" }} />}
-            value={createDurationFromHours(totalTimes[0])}
-            description="Siste tur"
-            primary_color={
-              totalTimes[0] > totalTimeMean ? "#6CE16A" : "#93032E"
-            }
-            secondary_value={createDurationFromHours(totalTimeMean)}
-            secondary_description={
-              "Gjennomsnitt siste " + totalTimes.length.toString() + " turer"
-            }
-            tooltip="Regnet ut basert på dine DEP og POR meldinger"
-            onClick={() => handleClick(BenchmarkType.TotalTime)}
-          />
-        </Box>
-      </Grid>
-      <Grid item xs={6}>
-        <Box>
-          <BenchmarkCard
-            title="Fisketid"
-            avatar={<PhishingRoundedIcon sx={{ color: "text.secondary" }} />}
-            value={createDurationFromHours(fishingHours[0])}
-            description="Siste tur"
-            primary_color={
-              fishingHours[0] > fishingHoursMean ? "#6CE16A" : "#93032E"
-            }
-            secondary_value={createDurationFromHours(fishingHoursMean)}
-            secondary_description={
-              "Gjennomsnitt siste " + fishingHours.length.toString() + " turer"
-            }
-            tooltip="Regnet ut basert på dine fangstmeldinger"
-            onClick={() => handleClick(BenchmarkType.FishingHours)}
-          />
-        </Box>
-      </Grid>
-      <Grid item xs={6}>
-        <Box>
-          <BenchmarkCard
-            title="Fiskedistanse"
-            avatar={<StraightenRoundedIcon sx={{ color: "text.secondary" }} />}
-            value={(fishingDistanceMean > 1852
-              ? fishingDistance[0] / 1852
-              : fishingDistance[0]
-            ).toFixed(1)}
-            description="Siste tur"
-            primary_color={
-              fishingDistance[0] > fishingDistanceMean ? "#6CE16A" : "#93032E"
-            }
-            secondary_value={(fishingDistanceMean > 1852
-              ? fishingDistanceMean / 1852
-              : fishingDistanceMean
-            ).toFixed(1)}
-            secondary_description={
-              "Gjennomsnitt siste " +
-              fishingDistance.length.toString() +
-              " turer"
-            }
-            metric={fishingDistanceMean > 1852 ? "nautiske mil" : "meter"}
-            tooltip="Regnet ut basert på dine fangstmeldinger"
-            onClick={() => handleClick(BenchmarkType.FishingDistance)}
-          />
-        </Box>
-      </Grid>
-      <Grid item xs={6}>
-        <Box>
-          <BenchmarkCard
-            title="Total vekt"
-            avatar={<ScaleRoundedIcon sx={{ color: "text.secondary" }} />}
-            value={(fishingWeightMean > 1000
-              ? fishingWeight[0] / 1000
-              : fishingWeight[0]
-            ).toFixed(1)}
-            description="Siste tur"
-            primary_color={
-              fishingWeight[0] > fishingWeightMean ? "#6CE16A" : "#93032E"
-            }
-            secondary_value={(fishingWeightMean > 1000
-              ? fishingWeightMean / 1000
-              : fishingWeightMean
-            ).toFixed(1)}
-            secondary_description={
-              "Gjennomsnitt siste " + fishingWeight.length.toString() + " turer"
-            }
-            metric={fishingWeightMean > 1000 ? "tonn" : "kilo"}
-            tooltip="Data basert på levert vekt"
-            onClick={() => handleClick(BenchmarkType.FishingWeight)}
-          />
-        </Box>
-      </Grid>
-      <BenchmarkModal />
+    container
+    spacing={3}
+    // justifyContent="center"
+    // direction ="column"
+    alignItems="center"
+   >
+    <Grid item xs={3}>
+      <Box>
+        <BenchmarkCard title = "Total time" value={450} secondary_value={337}/>
+      </Box>
     </Grid>
+    <Grid item xs={3}>
+      <Box>
+        <BenchmarkCard title = "Total distance" value={450} secondary_value={337}/>
+      </Box>
+    </Grid> 
+    <Grid item xs={3}>
+      <Box>
+        <BenchmarkCard title = "Total fuel" value={450} secondary_value={337}/>
+      </Box>
+    </Grid>       
+   </Grid>
   );
 };
