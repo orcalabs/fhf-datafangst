2b197763a0025ab3bb78447a834cc7755979f5df
Vetle Hofsøy-Woie <vetle.hofsoy-woie@sintef.no>
2023-10-13 14:13:38 +0200

    Change color of modal and add theme for charts

--- fhf-datafangst-client/src/components/Benchmark/Graphs/SpeciesHistogram.tsx
+++ fhf-datafangst-client/src/components/Benchmark/Graphs/SpeciesHistogram.tsx
@@ -27,7 +27,6 @@ import {
 } from "store/benchmark";
 import { Catch } from "models";
 import { Theme } from "./ChartsTheme";
-import ReactEChart from "echarts-for-react";
 
 const sumObjectValues = (hauls: (Haul | Delivery)[]) => {
   const res: Record<number, number> = {};
@@ -141,9 +140,8 @@ export const SpeciesHistogram: FC = () => {
           </Button>
         </ButtonGroup>
         {data && (
-
-          <ReactEChart
-            option={datasetOption(data, prevData, species)}
+          <Graph
+            options={datasetOption(data, prevData, species)}
             theme={Theme}
           />
         )}
