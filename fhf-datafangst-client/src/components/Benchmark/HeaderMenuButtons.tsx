7c279af3b4f8ebefcfda03e911a1db5200047bc4
Erling Devold <erling.devold@sintef.no>
2023-10-13 09:30:34 +0200

    Fixed linting

--- fhf-datafangst-client/src/components/Benchmark/HeaderMenuButtons.tsx
+++ fhf-datafangst-client/src/components/Benchmark/HeaderMenuButtons.tsx
@@ -1,5 +1,5 @@
 import { ArrowBackIos } from "@mui/icons-material";
-import { Box, Button, IconButton } from "@mui/material";
+import { Box, IconButton } from "@mui/material";
 import { FC } from "react";
 import { useNavigate } from "react-router-dom";
 
