c499a0de4e30ae21a9fda39a4e68d9849c62c0e7
Markus Bergum Hilbertsen <markus@orcalabs.no>
2023-10-16 13:33:04 +0200

    chore: fix error & simplify code

--- fhf-datafangst-client/src/store/benchmark/actions.ts
+++ fhf-datafangst-client/src/store/benchmark/actions.ts
@@ -2,7 +2,7 @@ import { createAction, createAsyncThunk } from "@reduxjs/toolkit";
 import * as Api from "api";
 import { BenchmarkDataSource, BenchmarkModalParams } from "./state";
 
-export interface benchmarkHistoricParams {
+export interface BenchmarkHistoricParams {
   metric: string;
   xAxis: string[];
   data: number[];
@@ -15,7 +15,7 @@ export const getBenchmarkData = createAsyncThunk(
 export const setBenchmarkModal = createAction<BenchmarkModalParams | undefined>(
   "benchmark/setBenchmarkModal",
 );
-export const setBenchmarkHistoric = createAction<benchmarkHistoricParams>(
+export const setBenchmarkHistoric = createAction<BenchmarkHistoricParams>(
   "benchmark/setBenchmarkHistoric",
 );
 
