ea042af5eb8cf5c151e2aa19370b33684856342a
Kim Hartvedt Andreassen <kim@orcalabs.no>
2024-10-09 14:02:20 +0200

    style: fix icons in TripsMenu

--- fhf-datafangst-client/src/assets/icons/index.ts
+++ fhf-datafangst-client/src/assets/icons/index.ts
@@ -1,4 +1,5 @@
 export { default as CoastlineIcon } from "./coastline";
+export { default as DeliveryIcon } from "./delivery";
 export { default as DeliveryPointIcon } from "./delivery-point";
 export { default as FishIcon } from "./fish";
 export { default as FishLocationIcon } from "./fish-location";
