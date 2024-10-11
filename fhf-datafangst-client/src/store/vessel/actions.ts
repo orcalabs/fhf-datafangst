12db4423e59dda27e85044017640052cc4bf1806
Kim Hartvedt Andreassen <kim@orcalabs.no>
2024-10-11 14:47:07 +0200

    feat: add api/store support for vessel benchmarks

--- fhf-datafangst-client/src/store/vessel/actions.ts
+++ fhf-datafangst-client/src/store/vessel/actions.ts
@@ -2,3 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import * as Api from "api";
 
 export const getVessels = createAsyncThunk("vessel/getVessels", Api.getVessels);
+export const getVesselBenchmarks = createAsyncThunk(
+  "vessel/getVesselBenchmarks",
+  Api.getVesselBenchmarks,
+);
--- fhf-datafangst-client/src/store/vessel/state.ts
+++ fhf-datafangst-client/src/store/vessel/state.ts
@@ -1,13 +1,15 @@
-import { Vessel } from "generated/openapi";
+import { Vessel, VesselBenchmarks } from "generated/openapi";
 
 export interface VesselState {
   vessels?: Vessel[];
   vesselsByCallsign?: Record<string, Vessel>;
   vesselsByFiskeridirId?: Record<number, Vessel>;
+  vesselBenchmarks?: VesselBenchmarks;
 }
 
 export const initialVesselState: VesselState = {
   vessels: undefined,
   vesselsByFiskeridirId: undefined,
   vesselsByCallsign: undefined,
+  vesselBenchmarks: undefined,
 };
