e4a94743b44fcbdc6c2bfe4e9d989845b5cd4d1c
Kim Hartvedt Andreassen <kim@orcalabs.no>
2025-01-10 09:31:05 +0100

    feat: add vessel fuel consumption api

--- fhf-datafangst-client/src/api/vesselsApi.ts
+++ fhf-datafangst-client/src/api/vesselsApi.ts
@@ -1,5 +1,13 @@
+import { formatISO } from "date-fns";
 import { VesselApi } from "generated/openapi";
 import { apiConfiguration, apiGet, axiosInstance } from "./baseApi";
+
+export interface FuelArgs {
+  token?: string;
+  startDate?: Date;
+  endDate?: Date;
+  callSignOverride?: string | null;
+}
 
 const api = new VesselApi(apiConfiguration, undefined, axiosInstance);
 
@@ -8,3 +16,19 @@ export const getVessels = async () =>
 
 export const getVesselBenchmarks = async (bwToken: string) =>
   apiGet(async () => api.routesV1VesselBenchmarksBenchmarks({ bwToken }));
+
+export const getEstimatedFuelConsumption = async (args: FuelArgs) =>
+  apiGet(async () =>
+    api.routesV1VesselFuel(
+      {
+        startDate: args.startDate
+          ? formatISO(args.startDate, { representation: "date" })
+          : undefined,
+        endDate: args.endDate
+          ? formatISO(args.endDate, { representation: "date" })
+          : undefined,
+        bwToken: args.token!,
+      },
+      { params: { call_sign_override: args.callSignOverride } },
+    ),
+  );
