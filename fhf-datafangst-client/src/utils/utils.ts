64d3c8fb2a5062b37ae6bb7a5b6c4f85fbd7ec96
Kim Hartvedt Andreassen <kim@orcalabs.no>
2025-01-30 14:10:48 +0100

    refactor: move numberInputLimiter to utils

--- fhf-datafangst-client/src/utils/utils.ts
+++ fhf-datafangst-client/src/utils/utils.ts
@@ -306,3 +306,20 @@ export const getGearGroupsFromVessels = (vessels: Vessel[]) => {
 };
 
 export const fuelTonsToLiters = (fuelTons: number) => (fuelTons * 1000) / 0.85;
+
+// Prevents all input from keys not related to input of a natural number
+export const numberInputLimiter = (e: React.KeyboardEvent<HTMLDivElement>) => {
+  if (
+    !(e.key >= "0" && e.key <= "9") &&
+    e.key !== "Delete" &&
+    e.key !== "Backspace" &&
+    e.key !== "ArrowLeft" &&
+    e.key !== "ArrowRight" &&
+    e.key !== "Home" &&
+    e.key !== "End" &&
+    e.key !== "Shift"
+  ) {
+    e.preventDefault();
+    return;
+  }
+};
