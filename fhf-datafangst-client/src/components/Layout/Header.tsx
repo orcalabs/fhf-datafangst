baa61102bcfaf334a4397fec0e226a66037b886f
Kim Hartvedt Andreassen <kim@orcalabs.no>
2025-02-17 10:49:27 +0100

    refactor: make login button an avatar with menu

--- fhf-datafangst-client/src/components/Layout/Header.tsx
+++ fhf-datafangst-client/src/components/Layout/Header.tsx
@@ -1,9 +1,15 @@
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import LogoutIcon from "@mui/icons-material/Logout";
+import LogoutSharpIcon from "@mui/icons-material/LogoutSharp";
+import SettingsIcon from "@mui/icons-material/Settings";
 import {
   AppBar,
+  Avatar,
+  Box,
   Button,
   Divider,
+  ListItemIcon,
+  Menu,
+  MenuItem,
   Stack,
   Toolbar,
   Typography,
@@ -13,8 +19,8 @@ import { AboutUs, HeaderMenuButtons } from "components";
 import { AppPage } from "containers/App/App";
 import { useAuth } from "oidc-react";
 import { FC, useState } from "react";
-import { Link as RouterLink } from "react-router";
-import { selectIsLoggedIn, useAppSelector } from "store";
+import { Link, Link as RouterLink } from "react-router";
+import { selectBwUserProfile, selectIsLoggedIn, useAppSelector } from "store";
 
 export const HEADER_HEIGHT = 52;
 
@@ -24,10 +30,15 @@ export interface Props {
 
 export const Header: FC<Props> = ({ page }) => {
   const { signIn, signOutRedirect } = useAuth();
-
+  const bwProfile = useAppSelector(selectBwUserProfile);
   const loggedIn = useAppSelector(selectIsLoggedIn);
 
   const [aboutModalOpen, setAboutModalOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
 
   return (
     <AppBar
@@ -67,7 +78,7 @@ export const Header: FC<Props> = ({ page }) => {
 
         <Stack
           direction="row"
-          spacing={0.5}
+          spacing={1}
           sx={{ gridColumn: 3, justifySelf: "end" }}
           alignItems="center"
         >
@@ -79,28 +90,92 @@ export const Header: FC<Props> = ({ page }) => {
           </Button>
           <Divider
             orientation="vertical"
-            sx={{ bgcolor: "white", height: 12 }}
+            sx={{ bgcolor: "white", height: loggedIn ? 16 : 12 }}
           />
-          <Button
-            sx={{
-              justifySelf: "end",
-              color: "white",
-              textTransform: "none",
-              fontWeight: "bold",
-            }}
-            onClick={() => {
-              loggedIn ? signOutRedirect() : signIn();
-            }}
-            endIcon={
-              loggedIn ? (
-                <LogoutIcon />
-              ) : (
-                <AccountCircleIcon color="secondary" />
-              )
-            }
-          >
-            {loggedIn ? "Logg ut" : "Logg inn"}
-          </Button>
+          {!loggedIn && (
+            <Button
+              sx={{
+                justifySelf: "end",
+                color: "white",
+                textTransform: "none",
+                fontWeight: "bold",
+              }}
+              onClick={() => signIn()}
+              endIcon={<AccountCircleIcon color="secondary" />}
+            >
+              Logg inn
+            </Button>
+          )}
+          {loggedIn && (
+            <Box sx={{ pl: 1.5, pr: 1.5 }}>
+              <Avatar
+                onClick={handleMenu}
+                sx={{
+                  width: 30,
+                  height: 30,
+                  bgcolor: "secondary.main",
+                  cursor: "pointer",
+                  color: "white",
+                }}
+              >
+                {bwProfile?.user.firstName?.charAt(0)}
+              </Avatar>
+
+              <Menu
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorEl)}
+                onClose={() => setAnchorEl(null)}
+              >
+                {bwProfile && (
+                  <>
+                    <Stack direction="row" gap={1.5} sx={{ p: 2, pt: 1 }}>
+                      <Avatar>{bwProfile?.user.firstName?.charAt(0)}</Avatar>
+                      <Stack direction="column">
+                        <Typography>
+                          {bwProfile?.user.firstName +
+                            " " +
+                            bwProfile?.user.lastName}
+                        </Typography>
+                        <Typography variant="subtitle2">
+                          {bwProfile?.user.email}
+                        </Typography>
+                      </Stack>
+                    </Stack>
+                    <Divider sx={{ bgcolor: "primary.main", mb: 1 }} />
+                  </>
+                )}
+
+                <Link
+                  style={{ color: "black", textDecoration: "none" }}
+                  to={"https://www.barentswatch.no/minside/"}
+                  target="_blank"
+                >
+                  <MenuItem onClick={() => setAnchorEl(null)}>
+                    <ListItemIcon>
+                      <SettingsIcon fontSize="small" />
+                    </ListItemIcon>
+                    Innstillinger
+                  </MenuItem>
+                </Link>
+
+                <MenuItem onClick={() => signOutRedirect()}>
+                  <ListItemIcon>
+                    <LogoutSharpIcon fontSize="small" />
+                  </ListItemIcon>
+                  Logg ut
+                </MenuItem>
+              </Menu>
+            </Box>
+          )}
         </Stack>
       </Toolbar>
       <AboutUs open={aboutModalOpen} onClose={() => setAboutModalOpen(false)} />
