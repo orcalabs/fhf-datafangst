302503a65b20bff6b38169c89014f064cffbf40e
Kim Hartvedt Andreassen <kim@orcalabs.no>
2025-10-30 13:54:37 +0100

    chore: change vessel

--- fhf-datafangst-client/src/store/reducers.ts
+++ fhf-datafangst-client/src/store/reducers.ts
@@ -74,7 +74,7 @@ const baseBuilder = (builder: ActionReducerMapBuilder<AppState>) =>
       // Hijack Skomværfisk as a vessel for testing purposes.
       if (state.bwUser.user.email === "post@orcalabs.no") {
         state.bwUser.fiskInfoProfile = {
-          ircs: "LHBQ",
+          ircs: "LDDF",
           mmsi: 257842500,
           imo: -1,
           regNum: "",
