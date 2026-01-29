ac1e66d6e47ec56b91659394846d42820e1c633e
Kim Hartvedt Andreassen <kim@orcalabs.no>
2026-01-29 12:22:27 +0100

    feat: add date selection shortcuts

--- fhf-datafangst-client/src/components/SearchFilters/DateFilter.tsx
+++ fhf-datafangst-client/src/components/SearchFilters/DateFilter.tsx
@@ -1,7 +1,19 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Chip, Stack, Typography } from "@mui/material";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
-import { endOfDay, isValid, startOfDay } from "date-fns";
+import theme from "app/theme";
+import {
+  endOfDay,
+  endOfMonth,
+  endOfYear,
+  getYear,
+  isValid,
+  startOfDay,
+  startOfMonth,
+  startOfYear,
+  subMonths,
+  subYears,
+} from "date-fns";
 import { nb } from "date-fns/locale";
 import { FC, useState } from "react";
 
@@ -35,10 +47,11 @@ interface Props {
   value?: DateRange;
   onChange: (_?: DateRange) => void;
   validateRange?: boolean;
+  showShortCuts?: boolean;
 }
 
 export const DateFilter: FC<Props> = (props) => {
-  const { value, onChange, validateRange } = props;
+  const { value, onChange, validateRange, showShortCuts } = props;
   const [error, setError] = useState<boolean>(false);
 
   const handleStartDateChange = (date: Date | null) => {
@@ -63,65 +76,125 @@ export const DateFilter: FC<Props> = (props) => {
         Dato
       </Typography>
       <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={nb}>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            "& .MuiButtonBase-root": {
-              borderRadius: 0,
-              "&:hover": {
+        <Stack spacing={1.5}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              "& .MuiButtonBase-root": {
                 borderRadius: 0,
+                "&:hover": {
+                  borderRadius: 0,
+                },
+                "&.clearButton": {
+                  p: 0,
+                },
               },
-              "&.clearButton": {
-                p: 0,
+              "& .MuiIconButton-sizeMedium": {
+                color: "secondary.main",
               },
-            },
-            "& .MuiIconButton-sizeMedium": {
-              color: "secondary.main",
-            },
-            "& .MuiFormControl-root": {
-              width: "49%",
-            },
-            "& .MuiInputLabel-root": {
-              color: "black",
-            },
-          }}
-        >
-          <DatePicker
-            label={"Fra"}
-            slotProps={{
-              textField: {
-                size: "small",
-                error: validateRange && error,
-                helperText:
-                  validateRange &&
-                  error &&
-                  "Ugyldig datointervall. Fra-dato må være mindre enn Til-dato",
+              "& .MuiFormControl-root": {
+                width: "49%",
               },
-              popper: { disablePortal: true },
-              field: { clearable: true },
+              "& .MuiInputLabel-root": {
+                color: "black",
+              },
             }}
-            onChange={handleStartDateChange}
-            value={value?.rawStart ?? null}
-            minDate={new Date("2011-01-01")}
-            maxDate={new Date()}
-          />
-          <DatePicker
-            label={"Til"}
-            slotProps={{
-              textField: {
-                size: "small",
-                error: validateRange && error,
-              },
-              popper: { disablePortal: true },
-              field: { clearable: true },
-            }}
-            value={value?.rawEnd ?? null}
-            minDate={new Date("2011-01-01")}
-            maxDate={new Date()}
-            onChange={handleEndDateChange}
-          />
-        </Box>
+          >
+            <DatePicker
+              label={"Fra"}
+              slotProps={{
+                textField: {
+                  size: "small",
+                  error: validateRange && error,
+                  helperText:
+                    validateRange &&
+                    error &&
+                    "Ugyldig datointervall. Fra-dato må være mindre enn Til-dato",
+                },
+                popper: { disablePortal: true },
+                field: { clearable: true },
+              }}
+              onChange={handleStartDateChange}
+              value={value?.rawStart ?? null}
+              minDate={new Date("2011-01-01")}
+              maxDate={new Date()}
+            />
+            <DatePicker
+              label={"Til"}
+              slotProps={{
+                textField: {
+                  size: "small",
+                  error: validateRange && error,
+                },
+                popper: { disablePortal: true },
+                field: { clearable: true },
+              }}
+              value={value?.rawEnd ?? null}
+              minDate={new Date("2011-01-01")}
+              maxDate={new Date()}
+              onChange={handleEndDateChange}
+            />
+          </Box>
+          {showShortCuts && (
+            <Stack
+              direction="row"
+              spacing={1}
+              sx={{
+                "& .MuiChip-root": {
+                  ":hover": {
+                    bgcolor: theme.palette.grey[300],
+                  },
+                },
+              }}
+            >
+              <Chip
+                label="Denne måneden"
+                onClick={() =>
+                  onChange(new DateRange(startOfMonth(new Date()), new Date()))
+                }
+              />
+              <Chip
+                label="Forrige måned"
+                onClick={() =>
+                  onChange(
+                    new DateRange(
+                      startOfMonth(subMonths(new Date(), 1)),
+                      endOfMonth(subMonths(new Date(), 1)),
+                    ),
+                  )
+                }
+              />
+              <Chip
+                label="Siste 3 måneder"
+                onClick={() =>
+                  onChange(
+                    new DateRange(
+                      startOfMonth(subMonths(new Date(), 3)),
+                      endOfMonth(subMonths(new Date(), 1)),
+                    ),
+                  )
+                }
+              />
+              {[subYears(new Date(), 2), subYears(new Date(), 1)].map(
+                (yearDate, i) => (
+                  <Chip
+                    key={i}
+                    label={getYear(yearDate)}
+                    onClick={() =>
+                      onChange(
+                        new DateRange(
+                          startOfYear(yearDate),
+                          endOfYear(yearDate),
+                        ),
+                      )
+                    }
+                  />
+                ),
+              )}
+            </Stack>
+          )}
+        </Stack>
       </LocalizationProvider>
     </>
   );
