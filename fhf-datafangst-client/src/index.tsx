226b6b26476406384938571b61a777fb0f96a946
Markus Bergum Hilbertsen <markus@orcalabs.no>
2026-03-17 12:38:22 +0100

    fix: memory leak during auth renewal

--- fhf-datafangst-client/src/index.tsx
+++ fhf-datafangst-client/src/index.tsx
@@ -1,4 +1,5 @@
 import { StyledEngineProvider, ThemeProvider } from "@mui/material";
+import { authConfig } from "app/auth";
 import theme from "app/theme";
 import "overlayscrollbars/overlayscrollbars.css";
 import { createRoot } from "react-dom/client";
@@ -10,22 +11,31 @@ import { App } from "./containers/App/App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-const container = document.getElementById("root");
-const root = createRoot(container!);
+if (window !== window.top) {
+  // `window !== window.top` means that the app is currently being rendered inside an iframe,
+  // which happens during auth token renewal.
+  authConfig.userManager!.signinSilentCallback();
+  if (window.frameElement) {
+    window.parent.document.body.removeChild(window.frameElement);
+  }
+} else {
+  const container = document.getElementById("root");
+  const root = createRoot(container!);
 
-root.render(
-  <StyledEngineProvider injectFirst>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </BrowserRouter>
-    </ThemeProvider>
-  </StyledEngineProvider>,
-);
+  root.render(
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </BrowserRouter>
+      </ThemeProvider>
+    </StyledEngineProvider>,
+  );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
+}
