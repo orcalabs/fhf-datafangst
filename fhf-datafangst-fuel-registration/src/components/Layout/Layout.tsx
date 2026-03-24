b28352e30a2c1b6eaec0fd88144bcb527545bc24
Kim Hartvedt Andreassen <kim@orcalabs.no>
2026-03-24 12:52:08 +0100

    style: remove overlayscrollbars

--- fhf-datafangst-fuel-registration/src/components/Layout/Layout.tsx
+++ fhf-datafangst-fuel-registration/src/components/Layout/Layout.tsx
@@ -1,5 +1,4 @@
 import { Box } from "@mui/material";
-import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
 import React, { ReactNode } from "react";
 interface Props {
   children: ReactNode;
@@ -7,20 +6,20 @@ interface Props {
 
 export const Layout: React.FC<Props> = ({ children }) => {
   return (
-    <OverlayScrollbarsComponent
-      className="overlayscrollbars-react"
-      options={{ scrollbars: { theme: "os-theme-light" } }}
-      defer
+    // <OverlayScrollbarsComponent
+    //   className="overlayscrollbars-react"
+    //   options={{ scrollbars: { theme: "os-theme-light" } }}
+    //   defer
+    // >
+    <Box
+      sx={{
+        height: ["100vh", "100dvh"],
+        width: "100%",
+        bgcolor: "rgb(237, 240, 243)",
+      }}
     >
-      <Box
-        sx={{
-          height: ["100vh", "100dvh"],
-          width: "100%",
-          bgcolor: "rgb(237, 240, 243)",
-        }}
-      >
-        <main>{children}</main>
-      </Box>
-    </OverlayScrollbarsComponent>
+      <main>{children}</main>
+    </Box>
+    //
   );
 };
